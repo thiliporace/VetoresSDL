e14fa099d6623cc7dd12728daa6814837821140e
Thiago Leandro Liporace <85422792+thiliporace@users.noreply.github.com>
2025-05-19 14:14:00 -0300

    Initial Commit

---
+++ VetoresSDL/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  VetoresSDL
+//
+//  Created by Thiago Liporace on 19/05/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
